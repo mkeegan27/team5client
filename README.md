464f9bbe4da01b911d9f3762f4a1d9cdd66aa81a
Michael Keegan <mekeegan@umass.edu>
2017-03-26 15:26:29 -0400

    react wrapper should be ready to go

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # 320 Team 5
 
 This repository so far will hold the client-side of the application.
+
+TO USE:
+    npm install
+    npm run serve
+
+
+Then navigate to localhost:8080 in your browser.
--- app/app.js
+++ app/app.js
@@ -1,1 +1,47 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import DoughnutExample from './components/doughnut';
+import DynamicDoughnutExample from './components/dynamic-doughnut';
+import PieExample from './components/pie';
+import LineExample from './components/line';
+import BarExample from './components/bar';
+import HorizontalBarExample from './components/horizontalBar';
+import RadarExample from './components/radar';
+import PolarExample from './components/polar';
+import BubbleExample from './components/bubble';
+import MixedDataExample from './components/mix';
+import MyExample from './components/mytest';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<hr />
+				<DoughnutExample />
+				<hr />
+				<DynamicDoughnutExample />
+				<hr />
+				<PieExample />
+				<hr />
+				<LineExample />
+				<hr />
+				<BarExample />
+				<hr />
+				<HorizontalBarExample />
+				<hr />
+				<RadarExample />
+				<hr />
+				<PolarExample />
+				<hr />
+				<BubbleExample />
+				<hr />
+				<MixedDataExample />
+				<hr />
+				<MyExample />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
